aaacee56b93b08306847c18ce16e0ff38c90bb02
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-11-30 16:37:36 +0000

    added a dynamic icon to the toggle button

--- script.js
+++ script.js
@@ -7,11 +7,21 @@ function toggleNavbar(e) {
   if (nav.classList.contains("show")) {
     nav.classList.remove("show");
     toggle.classList.remove("moveToggle");
+    toggle.innerHTML = '<i class="i fa fa-bars fa-2x"></i>';
   } else {
     nav.classList.add("show");
     toggle.classList.add("moveToggle");
+    toggle.innerHTML = '<i class="fa-solid fa-x fa-2x"></i>';
   }
+  e.stopPropagation();
 }
 
 //EVENT LISTENERS
 toggle.addEventListener("click", toggleNavbar);
+document.addEventListener("click", (e) => {
+  if (!nav.contains(e.target)) {
+    nav.classList.remove("show");
+    toggle.classList.remove("moveToggle");
+    toggle.innerHTML = '<i class="i fa fa-bars fa-2x"></i>';
+  }
+});
