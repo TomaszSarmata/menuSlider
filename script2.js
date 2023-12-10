159d678b776646fac96159b93080db10c6c8f97f
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-12-10 09:14:40 +0000

    implemented the logic to show and hide the elements

--- script2.js
+++ script2.js
@@ -1,1 +1,22 @@
 //Solutions from the course work
+const toggle = document.getElementById("toggle");
+const closeBtn = document.getElementById("close");
+const openBtn = document.getElementById("open");
+const modal = document.getElementById("modal");
+
+alert("here");
+
+//Toggle nav
+//here the diff is that we could've used the .toggle method to toggle the class. And also the entire body shifts right
+toggle.addEventListener("click", () =>
+  document.body.classList.toggle("show-nav")
+);
+
+//Show modal
+openBtn.addEventListener("click", () => modal.classList.add("show-modal"));
+//Hide modal
+closeBtn.addEventListener("click", () => modal.classList.remove("show-modal"));
+//Hide modal on outside click
+window.addEventListener("click", (e) =>
+  e.target == modal ? modal.classList.remove("show-modal") : false
+);
